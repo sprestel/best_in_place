14035202fae64d3b8b375ed1a8a73f43078b60f7
Sebastian Prestel <sebastian.prestel@yahoo.de>
2011-09-13 17:49:44 +0200

    additional_attributes for custom attributes in post request

--- lib/best_in_place/helper.rb
+++ lib/best_in_place/helper.rb
@@ -20,12 +20,15 @@ module BestInPlace
         collection = opts[:collection].to_json
       end
       opts[:nil] = opts[:alt] if opts[:alt]
+
+      field.gsub!("local_", "") unless opts[:additional_params].blank?
       out = "<span class='best_in_place'"
       out << " id='best_in_place_#{object.class.to_s.gsub("::", "_").underscore}_#{field}'"
       out << " data-url='#{opts[:path].blank? ? url_for(object).to_s : url_for(opts[:path])}'"
       out << " data-object='#{object.class.to_s.gsub("::", "_").underscore}'"
       out << " data-collection='#{collection}'" unless collection.blank?
       out << " data-attribute='#{field}'"
+      out << " data-addtional_attributes='#{opts[:additional_params]}'" unless opts[:additional_params].blank?
       out << " data-activator='#{opts[:activator]}'" unless opts[:activator].blank?
       out << " data-nil='#{opts[:nil].to_s}'" unless opts[:nil].blank?
       out << " data-type='#{opts[:type].to_s}'"
